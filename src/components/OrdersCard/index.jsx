52765c4a458d558297ed6a0ee6aebc574bb33995
Brayan-Alexis <brayanzxd@gmail.com>
2023-12-08 13:32:41 -0700

    Add date and time to OrdersCard component 📄

--- src/components/OrdersCard/index.jsx
+++ src/components/OrdersCard/index.jsx
@@ -1,11 +1,37 @@
+// import PropTypes from "prop-types";
+import { useGlobalContext } from "../../context";
+import { getCurrentDateTime } from "../../utils";
 
 function OrdersCard({
+  totalPrice,
+  id,
+  title,
+  price,
+  category,
+  images,
+  discountPercentage,
+  quantity,
 }) {
+  OrdersCard.propTypes = {
+    // title: PropTypes.string.isRequired,
+    // category: PropTypes.string.isRequired,
+    // price: PropTypes.number.isRequired,
+    // images: PropTypes.arrayOf(PropTypes.string).isRequired,
+    // quantity: PropTypes.number.isRequired,
+  };
+
+  const {
+    productTotalPrice,
+  } = useGlobalContext();
 
   return (
     <div className="my-2 mr-2">
       <div className={"flex justify-between items-center mb-3 border border-green-500"}>
         <p>
+          {/* <span>Date: {new Date().toLocaleDateString()}</span> */}
+          <span>Date: {getCurrentDateTime()}</span>
+          <span className="font-semibold"> -  Total: {quantity}</span>
+          <span className="font-semibold">{totalPrice}</span>
         </p>
       </div>
     </div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -7,8 +7,21 @@ export const totalPrice = (products) => {
   return products.reduce(
     (total, product) => {
       const discountedPrice = (product.price * (1 - product.discountPercentage / 100)).toFixed(2);
-      return total + discountedPrice * product.quantity.toFixed(2);
+      return (total + discountedPrice * product.quantity).toFixed(2);
     },
     0
   );
+};
+
+/**
+ * This function obtains current date and time
+ * @returns {number} dateTime: Current date and time
+ */
+export const getCurrentDateTime = () => {
+  let today = new Date();
+  let date = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  let dateTime = date + " " + time;
+
+  return dateTime;
 };
